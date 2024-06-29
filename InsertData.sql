16331f305a68a77a0def322ccaafff9dbfd2be48
fakhrimhd <144667437+fakhrimhd@users.noreply.github.com>
2024-06-29 12:36:47 +0200

    Update InsertData.sql
    - Add insert for orders table

--- InsertData.sql
+++ InsertData.sql
@@ -1,7 +1,4 @@
 -- Insert data into the Supplier table
--- Enable IDENTITY_INSERT for the supplier table
-SET IDENTITY_INSERT supplier ON;
-
 INSERT INTO supplier (supplier_id, company_name, address, city, province, postal_code, country)
 VALUES 
 (001, 'Supplier A', 'Address A', 'Jakarta', 'DKI Jakarta', '10110', 'IDN'),
@@ -15,13 +12,8 @@ VALUES
 (009, 'Supplier I', 'Address I', 'Yogyakarta', 'DIY', '55000', 'IDN'),
 (010, 'Supplier J', 'Address J', 'Cileungsi', 'Jawa Barat', '16800', 'IDN');
 
--- Disable IDENTITY_INSERT after the insert operation
-SET IDENTITY_INSERT supplier OFF;
 
 -- Insert data into the Material table
--- Enable IDENTITY_INSERT for the supplier table
-SET IDENTITY_INSERT material ON;
-
 INSERT INTO material (material_id, name, description, unit)
 VALUES 
 (1200, 'Bitumen', 'Bitumen type A', 'Cubic Meters'),
@@ -29,26 +21,23 @@ VALUES
 (1310, 'Coarse Aggregate', 'Aggregate A', 'Tons'),
 (1400, 'Solar', 'Industrial solar', 'Liters');
 
--- Disable IDENTITY_INSERT after the insert operation
-SET IDENTITY_INSERT material OFF;
-
 -- Insert data into the Production Unit table
--- Enable IDENTITY_INSERT for the supplier table
-SET IDENTITY_INSERT production_unit ON;
-
 INSERT INTO production_unit (unit_id, unit_name, address, city, province, postal_code, country)
 VALUES 
 (201, 'Unit A', 'Address unit A', 'Medan', 'Sumatra Utara', '20000', 'IDN'),
 (202, 'Unit B', 'Address unit B', 'Bandung', 'Jawa Barat', '40100', 'IDN');
 
--- Disable IDENTITY_INSERT after the insert operation
-SET IDENTITY_INSERT production_unit OFF;
-
 -- Insert data into the Contract table
-INSERT INTO contract (supplier_id, material_id, unit_id, quantity, price_per_unit, start_date, end_date, supply_date, status)
+INSERT INTO contract (contract_id, supplier_id, material_id, unit_id, quantity, price_per_unit, start_date, end_date, supply_date, status)
 VALUES 
-(001, 1200, 201, 100, 75.00, '2020-01-01', '2020-12-31', '2020-01-15', 'Active'),
-(002, 1300, 202, 50, 120.00, '2020-02-01', '2020-11-30', '2020-02-20', 'Active');
+(1010120, 001, 1200, 201, 100, 75.00, '2020-01-01', '2020-12-31', '2020-01-15', 'Active'),
+(2010120, 002, 1300, 202, 50, 120.00, '2020-02-01', '2020-11-30', '2020-02-20', 'Active');
+
+-- Insert data into the Orders table
+INSERT INTO orders (order_id, contract_id, unit_id, order_date, required_date, shipped_date, status)
+VALUES 
+(1, 1010120, 201, '2020-01-10', '2022-01-20', '2022-01-15', 'Completed'),
+(2, 1010120, 201, '2020-02-15', '2022-03-01', '2022-02-25', 'Pending');
 
 -- Insert data into the Unit Inventory table
 INSERT INTO unit_inventory (unit_id, material_id, quantity, last_updated)
