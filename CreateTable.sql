939513b2df7eb92d7daa936108693da02867eb18
fakhrimhd <144667437+fakhrimhd@users.noreply.github.com>
2024-07-06 07:32:38 +0200

    Add files via upload

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,33 +1,33 @@
 -- Creating the Supplier table
 CREATE TABLE supplier (
     supplier_id INT PRIMARY KEY,
-    company_name NVARCHAR(100),
-    contact_name NVARCHAR(100),
-    contact_title NVARCHAR(50),
-    address NVARCHAR(255),
-    city NVARCHAR(100),
-    province NVARCHAR(100),
-    postal_code NVARCHAR(20),
-    country NVARCHAR(100),
+    company_name VARCHAR(100),
+    contact_name VARCHAR(100),
+    contact_title VARCHAR(50),
+    address VARCHAR(255),
+    city VARCHAR(100),
+    province VARCHAR(100),
+    postal_code VARCHAR(20),
+    country VARCHAR(100),
 )
 
 -- Creating the Material table
 CREATE TABLE material (
     material_id INT PRIMARY KEY,
-    name NVARCHAR(100),
-    description NVARCHAR(255),
-    unit NVARCHAR(50)
+    name VARCHAR(100),
+    description VARCHAR(255),
+    unit VARCHAR(50)
 )
 
 -- Creating the Production Unit table
 CREATE TABLE production_unit (
     unit_id INT PRIMARY KEY,
-    unit_name NVARCHAR(100),
-    address NVARCHAR(255),
-    city NVARCHAR(100),
-    province NVARCHAR(100),
-    postal_code NVARCHAR(20),
-    country NVARCHAR(100)
+    unit_name VARCHAR(100),
+    address VARCHAR(255),
+    city VARCHAR(100),
+    province VARCHAR(100),
+    postal_code VARCHAR(20),
+    country VARCHAR(100)
 )
 
 -- Creating the Contract table
@@ -42,7 +42,7 @@ CREATE TABLE contract (
     start_date DATE,
     end_date DATE,
     supply_date DATE,
-    status NVARCHAR(50),
+    status VARCHAR(50),
     FOREIGN KEY (supplier_id) REFERENCES supplier(supplier_id),
     FOREIGN KEY (material_id) REFERENCES material(material_id),
     FOREIGN KEY (unit_id) REFERENCES production_unit(unit_id)
@@ -56,13 +56,14 @@ CREATE TABLE orders (
     order_date DATE,
     required_date DATE,
     shipped_date DATE,
-    status NVARCHAR(50),
+    status VARCHAR(50),
     FOREIGN KEY (contract_id) REFERENCES contract(contract_id),
     FOREIGN KEY (unit_id) REFERENCES production_unit(unit_id)
 )
 
 -- Creating the Unit Inventory table
 CREATE TABLE unit_inventory (
+    inventory_id INT PRIMARY KEY,
     unit_id INT,
     material_id INT,
     quantity DECIMAL(18, 2),
